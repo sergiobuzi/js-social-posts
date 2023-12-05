387991c53b2037c1f91ed09a6a94d9e18c7e2089
SergioBuzi <sergio.buzi97@gmail.com>
2023-12-05 14:23:57 +0100

    script: all posts created by js

--- main.js
+++ main.js
@@ -57,4 +57,48 @@ const posts = [
 ];
 
 
+const container = document.getElementById("container");
+
+posts.forEach((element, index,) => {
+    console.log(element, index,);
+    
+    let post = 
+    
+        `
+        <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${element.author.image}" alt="${element.author.name}">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${element.author.name}</div>
+                    <div class="post-meta__time">${element.created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${element.content}</div>
+        <div class="post__image">
+            <img src="${element.media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="${element.id}">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>
+        `
+
+    //inserisco i blocchi all'interno dell'html
+    container.innerHTML += post;
+
+});
 
